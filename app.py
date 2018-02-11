8915da08c6b8ae5f89ac9e623fc39a92e0257b84
Aman Roy <royaman8757@gmail.com>
2018-02-11 11:03:11 +0530

    error handling

--- app.py
+++ app.py
@@ -8,5 +8,16 @@ def index():
 	"""Index page"""
 	pass
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 404	
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
+
 if __name__ == '__main__':
     app.run()
